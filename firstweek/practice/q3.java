3730df48bd3e95654145cca931515197a02b2af5
yashwanthkanthraj <yashwanthkanthraj1@gmail.com>
2025-03-17 21:18:09 +0530

    practice questions

---
+++ firstweek/practice/q3.java
@@ -0,0 +1,22 @@
+package firstweek.practice;
+
+import java.util.Scanner;
+
+//Check if a given year is a leap year (divisible by 4, not by 100 unless also by 400)
+                                     //((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)).
+public class q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the year -");
+        int year = scanner.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("the given year - " + year + " is a leap year");
+
+        } else {
+            System.out.println(year + " is not a leap year !");
+        }
+        scanner.close();
+    }
+
+}
